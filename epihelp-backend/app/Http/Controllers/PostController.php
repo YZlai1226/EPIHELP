f47d75cbadcf797661e48cae23ce9db5d722d98f
Karen <yong-zhen.lai@epitech.eu>
2022-05-30 11:46:23 +0200

    edit postController and categoryController

--- epihelp-backend/app/Http/Controllers/PostController.php
+++ epihelp-backend/app/Http/Controllers/PostController.php
@@ -5,21 +5,67 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-// class PostController extends Controller
-// {
-//     //
-// }
-
-
-
-
 class PostController extends Controller
 {
-   public function show($slug)
-   {
-       return view('post', [
-           'post' => Post::where('slug', '=', $slug)->first()
-       ]);
-   }
+//    public function show($slug)
+//    {
+//        return view('post', [
+//            'post' => Post::where('slug', '=', $slug)->first()
+//        ]);
+//    }
+/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
