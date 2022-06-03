14c607893e9adba4954710e187aaa049e0a124f5
Nicolas Bretécher <bretechern@gmail.com>
2022-06-03 16:48:54 +0200

    fix profile data fetching

--- epihelp-frontend/src/models/user.ts
+++ epihelp-frontend/src/models/user.ts
@@ -1,6 +1,30 @@
-export interface User {
-    email: string;
-    name: string;
-    token: string;
-    role?: string;
+export interface PostList {
+    title: string,
+    content: string,
+    category: string,
+    author_id: string,
+    author_name: string,
+    created_at: string,
+    updated_at: string,
+    number_of_comments?: number,
+    last_comment?: {
+        id: string,
+        author_id: string,
+        author_name: string,
+        content: string,
+        created_at: string,
+        updated_at: string
+    }
 }
+
+export interface User {
+        id: string,
+        name: string,
+        email: string,
+        password: string,
+        posts?: PostList[],
+        comments?: Array<Object>,
+        role?: string,
+        created_at: string,
+        updated_at: string,
+    }
