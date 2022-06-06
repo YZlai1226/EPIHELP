3e1760f00f39d739ce94d6b5f97d7b6f6cce6834
Nicolas Bretécher <bretechern@gmail.com>
2022-06-07 00:10:25 +0200

    add search bar and change layout

--- epihelp-frontend/src/components/Layouts/Navigation.tsx
+++ epihelp-frontend/src/components/Layouts/Navigation.tsx
@@ -15,7 +15,7 @@ const Navigation: React.FC<NavigationProps> = ({ user }) => {
   const [open, setOpen] = useState(false)
 
   return (
-    <nav className="bg-white border-b border-gray-100">
+    <nav className="bg-white border-b border-gray-100 shadow">
       {/* Primary Navigation Menu */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
