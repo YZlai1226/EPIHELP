aa2ea214c1d578bc4657066895d0ef23517e6740
Nicolas Bretécher <bretechern@gmail.com>
2022-06-07 11:39:19 +0200

    Re-add header

--- epihelp-frontend/src/components/Layouts/AppLayout.tsx
+++ epihelp-frontend/src/components/Layouts/AppLayout.tsx
@@ -13,11 +13,11 @@ const AppLayout: React.FC<AppLayoutProps> = ({ header, children }) => {
       <div className="min-h-screen bg-gray-100">
         <Navigation user={user} />
         {/* Page Heading */}
-        {/* <header className="bg-white shadow">
+        <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {header}
           </div>
-        </header> */}
+        </header>
         {/* Page Content */}
         <main>{children}</main>
       </div>
